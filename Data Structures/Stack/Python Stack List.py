3346f7a7f652ba4bc1a682d5666339e17f09e172
cacadosman <wetmanz23@gmail.com>
2017-07-05 20:32:58 +0700

    Add Python Stack List

---
+++ Data Structures/Stack/Python Stack List.py
@@ -0,0 +1,11 @@
+# So Simple (Use List)
+
+stack = []
+stack.append(10)
+stack.append(20)
+print stack.pop()
+print stack.pop()
+stack.append(30)
+stack.append(40)
+print stack.pop()
+print stack.pop()
