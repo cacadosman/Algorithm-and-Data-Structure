8e7f82f0fb129c72a99ee27c296cb190cb52c8cc
cacadosman <wetmanz23@gmail.com>
2017-07-06 16:23:13 +0700

    Add Python Bubble Sort

---
+++ Algorithms/Sorting/Python Bubble Sort.py
@@ -0,0 +1,12 @@
+def sort(list):
+    max = len(list)
+    for i in range(1,max):
+        for j in range(0,max-i):
+            if list[j] > list[j+1]:
+                temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+
+list = [3,5,2,7,8,6,4]
+sort(list)
+print list
